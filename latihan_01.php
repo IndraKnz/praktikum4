4352fa4aca0c4822857978f0d549d52440806436
indraknz <indrakesuma@students.amikom.ac.id>
2024-03-21 15:26:24 +0700

    22.11.4722

---
+++ latihan_01.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+ <title>Embedded Script</title>
+ <head>
+ <body>
+ <p>Program JavaScript Pertama Saya!</p>
+ <script type="JavaScript">
+ alert("Hello Dab!");
+ <script type="JavaScript"> hallo</script>.
+ </body>
+</html>
